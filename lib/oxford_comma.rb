0778c4f8827e3e4579c2a54b4fd0ba48b479e447
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-23 23:42:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,4 @@
 def oxford_comma(array)
-  tostring = ""
-  count = 0
-  array.each do |string|
-    if array.length == 1
-      tostring += string
-    elsif count == (array.length - 1)
-      tostring += " and "+string
-    else
-      tostring += ", "+string
-    end
-  end
+  tostring = array.join(", ")
   return tostring
 end
