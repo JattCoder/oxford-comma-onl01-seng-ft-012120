d1cfc0f51a791ea785f457ff15d6dd1c74d621de
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-23 23:38:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  tostring = ""
+  count = 0
+  array.each do |string|
+    if array.length == 1
+      tostring += string
+    elsif count == (array.length - 1)
+      tostring += " and "+string
+    else
+      tostring += ", "+string
+    end
+  end
 end
